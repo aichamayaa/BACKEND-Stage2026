5c96c6ac3809f74657c13dac7187a4aeff061198
jeffsawma <jeff.sawma@live.com>
2026-06-21 22:25:49 -0400

    Add colleges CRUD API

---
+++ SystemePlacement.Web/DTOs/Colleges/CollegeResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemePlacement.Web.DTOs.Colleges
+{
+    public class CollegeResponseDto
+    {
+        public int IdCollege { get; set; }
+        public string Nom { get; set; } = string.Empty;
+        public string Ville { get; set; } = string.Empty;
+        public bool Actif { get; set; }
+    }
+}
+
+// Décrit ce que l'API renvoie au client lors d'une demande d'informations sur un établissement d'enseignement supérieur
+
