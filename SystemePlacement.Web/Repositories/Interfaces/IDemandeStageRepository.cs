c97032ebb163219646586b434588f072c5aeefea
jeffsawma <jeff.sawma@live.com>
2026-08-02 23:35:21 -0400

    Fix internship request data and employer visibility

--- SystemePlacement.Web/Repositories/Interfaces/IDemandeStageRepository.cs
+++ SystemePlacement.Web/Repositories/Interfaces/IDemandeStageRepository.cs
@@ -6,8 +6,10 @@ public interface IDemandeStageRepository
 {
     Task<int?> GetIdEtudiantByUtilisateurAsync(int idUtilisateur);
     Task AddAsync(DemandeStage demande);
+    Task<DemandeStage?> GetByIdAsync(int idDemandeStage);
     Task<List<DemandeStage>> GetByEtudiantAsync(int idEtudiant);
     Task<List<DemandeStage>> GetByDomaineAsync(int idDomaine);
+    Task<bool> EmployeurHasOfferInDomainAsync(int idUtilisateur, int idDomaine);
     Task<string?> GetNomDomaineAsync(int idDomaine);
     Task<int?> GetIdCollegeByDomaineAsync(int idDomaine);
     Task<string?> GetNomEtudiantAsync(int idEtudiant);
